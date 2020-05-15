9bcfeb39ddfafd1eff8b7f83f759e67bcd0d6343
rasithaw <65367587+rasithaw@users.noreply.github.com>
2020-05-15 14:13:03 -0400

    Updated the code into a function

--- roullette.py
+++ roullette.py
@@ -1,34 +1,43 @@
 '''
-Double Or Nothing Strategy for Roullette 
+Martingale Strategy for Roullette 
+This simulates the probable outcomes when changing your bet/pot
+
 '''
 from random import seed
 from random import randint
-
+#inputs
 StartingBet = 1
-StartingPot = 100
+StartingPot = 1000
 NumOfTries = 100
-Pot = StartingPot
-Bet = StartingBet
-Round = 0
-RoulletteNum = 0
-BettedNumbers = [2,4,6,8,10,12,16,18,20,22,24,26,28,30,32,34,36]
+EvenBetNumbers = [2,4,6,8,10,12,16,18,20,22,24,26,28,30,32,34,36]
+WinningCap = 100
 
-for _ in range(NumOfTries):
-    RoulletteNum = randint(0, 36)
-    Round = Round + 1
-    if Pot>0 and Pot <StartingPot*1.2:
-        if RoulletteNum in BettedNumbers:
-            Pot = Pot + Bet 
-            print ('You win = ' + str(Bet))
-            Bet = StartingBet
-        Pot = Pot - Bet
-        print ('You lost =' + str(Bet))
-        Bet = Bet * 2
-    elif Pot >= StartingPot*1.2:
-        print('enough for today')
-        break
-    else:
-        print('you lost everything')
-        break
+def MartingaleRouletteStrategySim(StartingPot,StartingBet,WinningCap,BettedNumbers,NumOfTries):
+    #initializing variables
+    Bet = StartingBet
+    Pot = StartingPot
+    WinCapRatio = (WinningCap + StartingPot)/StartingPot
+    Round = 0
+    RoulletteNum = 0
     
-print('final winnings = ' + str(Pot - StartingPot) + ' in round = ' + str(Round))
+    for _ in range(NumOfTries):
+        RoulletteNum = randint(0, 36)
+        Round = Round + 1
+        if Pot>0 and Pot <StartingPot*WinCapRatio:
+            if RoulletteNum in BettedNumbers:
+                Pot = Pot + Bet 
+                print ('You win = ' + str(Bet))
+                Bet = StartingBet
+            Pot = Pot - Bet
+            print ('You lost =' + str(Bet))
+            Bet = Bet * 2
+        elif Pot >= StartingPot*WinCapRatio:
+            print('enough for today')
+            break
+        else:
+            print('you lost everything')
+            break
+    FinalWin = Pot - StartingPot
+    return [FinalWin,Round]
+
+print(MartingaleRouletteStrategySim(StartingPot,StartingBet,WinningCap,EvenBetNumbers,NumOfTries))
